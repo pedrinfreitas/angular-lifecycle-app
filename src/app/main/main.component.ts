84b1defba906c08e24f9f47e5b6bec4e6f1a76b3
Pedro Freitas <pedrinfreitas@gmail.com>
2020-10-10 12:40:36 -0300

    feat: edit *simples

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -14,22 +14,39 @@ export class MainComponent implements OnInit {
   age: number;
   food: string;
 
-  editClient: Client = null;
+  editClient: number = -1;
 
   constructor() {}
 
   ngOnInit(): void {}
 
   save() {
-    if (this.editClient == null) {
+    if (this.editClient == -1) {
       this.clients.push({
         name: this.name,
         age: this.age,
-        food: this.food
+        food: this.food,
       });
+    } else {
+      this.clients[this.editClient].name = this.name;
+      this.clients[this.editClient].age = this.age;
+      this.clients[this.editClient].food = this.food;
+      this.editClient = -1;
     }
-    this.name = "";
+
+    this.name = '';
     this.age = null;
-    this.food = "";
+    this.food = '';
+  }
+
+  edit(i: number) {
+    this.name = this.clients[i].name;
+    this.age = this.clients[i].age;
+    this.food = this.clients[i].food;
+    this.editClient = i;
+  }
+
+  delete(i: number) {
+    this.clients.splice(i, 1);
   }
 }
