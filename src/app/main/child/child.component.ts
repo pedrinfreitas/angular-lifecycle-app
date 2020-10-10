8706fe34d7f1f0b5a9811619c4e42940b53ea65a
Pedro Freitas <pedrinfreitas@gmail.com>
2020-10-10 11:11:26 -0300

    conhecendo os lifecycles hooks

--- src/app/main/child/child.component.ts
+++ src/app/main/child/child.component.ts
@@ -1,16 +1,46 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  SimpleChanges,
+  OnChanges,
+  OnDestroy,
+  AfterContentInit,
+  AfterViewInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css'],
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent
+  implements OnInit, OnChanges, AfterContentInit, AfterViewInit, OnDestroy {
   @Input() name: string;
   @Input() age: number;
   @Input() food: string;
 
-  constructor() {}
+  constructor() {
+    console.log(`${this.name} - constructor`);
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(`${this.name} - ngOnInit`);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(`${this.name} - ngOnChanges`);
+  }
+
+  ngAfterContentInit() {
+    console.log(`${this.name} - ngAfterContentInit`);
+  }
+
+  ngAfterViewInit() {
+    console.log(`${this.name} - ngAfterViewInit`);
+  }
+
+  ngOnDestroy() {
+    console.log(`${this.name} - ngOnDestroy`);
+  }
 }
