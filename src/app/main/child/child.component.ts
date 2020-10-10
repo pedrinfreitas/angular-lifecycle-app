651b57c83018eab193c3eb6793fbf6a21842011f
Pedro Freitas <pedrinfreitas@gmail.com>
2020-10-10 11:58:54 -0300

    analisando a ordem de execucao com eventos

--- src/app/main/child/child.component.ts
+++ src/app/main/child/child.component.ts
@@ -9,6 +9,12 @@ import {
   AfterViewInit,
 } from '@angular/core';
 
+export interface LifeCycleEvent {
+  id: number;
+  name: string;
+  color: string;
+}
+
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
@@ -20,27 +26,55 @@ export class ChildComponent
   @Input() age: number;
   @Input() food: string;
 
+  public events: LifeCycleEvent[] = [];
+  nextEventId: number = 0;
+
+  colors: string[] = ['accent', 'warn', 'primary'];
+
   constructor() {
     console.log(`${this.name} - constructor`);
+    this.newEvent("constructor");
   }
 
   ngOnInit(): void {
     console.log(`${this.name} - ngOnInit`);
+    this.newEvent("ngOnInit");
   }
 
   ngOnChanges(changes: SimpleChanges) {
     console.log(`${this.name} - ngOnChanges`);
+    this.newEvent("ngOnChanges");
   }
 
   ngAfterContentInit() {
     console.log(`${this.name} - ngAfterContentInit`);
+    this.newEvent("ngAfterContentInit");
   }
 
   ngAfterViewInit() {
     console.log(`${this.name} - ngAfterViewInit`);
+    this.newEvent("ngAfterViewInit");
   }
 
   ngOnDestroy() {
     console.log(`${this.name} - ngOnDestroy`);
+    this.newEvent("ngOnDestroy");
+  }
+
+  newEvent(name: string) {
+    let id = this.nextEventId++;
+    this.events.push({ 
+      id: id, 
+      name: name, 
+      color: this.colors[id%this.colors.length] 
+    });
+    console.log(`Cor-> ${id%this.colors.length}`);
+    
+    setTimeout(()=>{
+      let idx = this.events.findIndex(e => e.id==id);
+      if(idx >= 0){
+        this.events.splice(idx, 1);
+      }
+    }, 2000 + this.events.length*1000)
   }
 }
